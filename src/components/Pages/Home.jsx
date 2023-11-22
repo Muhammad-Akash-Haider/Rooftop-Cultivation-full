754e57e3caf99ccb0204893907b71340fdf17446
Majid ali <majidali37406@gmail.com>
2023-11-22 21:27:42 +0500

    checkout page done

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Ecommerce from "./Ecommerce";
-import Header from "./Header";
-import Nav from "./Nav";
-import Section from "./Section";
-import E2 from "./E2";
-import Footer from "./Footer";
-import E3 from "./E3"
+import Ecommerce from "../Ecommerce";
+import Header from "../Header";
+import Nav from "../Nav";
+import Section from "../Section";
+import E2 from "../E2";
+import Footer from "../Footer";
+import E3 from "../E3"
 
 function Home() {
     return (
