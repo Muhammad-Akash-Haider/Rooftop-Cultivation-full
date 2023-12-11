1368fa6d9b0a1ac09f3707d140e21d5da2a28ebc
Majid ali <majidali37406@gmail.com>
2023-12-11 20:10:09 +0500

    businss profile page

--- Frontend/src/components/Pages/Business.jsx
+++ Frontend/src/components/Pages/Business.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import Nav from '../Nav';
-import img12 from './images2/img6.jpg'
+
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Link, useParams } from 'react-router-dom'
+import {  useParams } from 'react-router-dom'
+import { IoLocationOutline } from "react-icons/io5";
+import { MdOutlinePhoneInTalk } from "react-icons/md";
 
 
 // Import Swiper styles
@@ -42,9 +44,9 @@ function Business() {
             <Nav />
 
 
-            <Swiper navigation={true} modules={[Navigation]} className="mySwiper "
-             style={{ height: "500px" }}
-             >
+            <Swiper navigation={true} modules={[Navigation]} className=" mySwiper"
+                style={{ height: "500px"}}
+            >
 
                 {fetchnurseryData.gallery && fetchnurseryData.gallery.split(',').map((image, index) => (
                     <SwiperSlide key={index} >
@@ -58,40 +60,29 @@ function Business() {
             </Swiper>
 
 
-            <div className='flex justify-between w-full px-32 mt-16'>
+            <div className='flex justify-between w-full px-24 mt-16'>
                 <div className='flex justify-center text-center'>
-
                     <img
-                        className='w-20 h-20 mx-auto rounded-3xl'
+                        className='w-20 h-20 mx-auto rounded-full'
                         src={`http://localhost:5000/uploads/${fetchnurseryData && fetchnurseryData.gallery ? fetchnurseryData.gallery.split(',')[0] : ''}`}
                         alt=""
                     />
-
-
-                    <h1 className='items-center pt-6 text-xl font-bold' >{fetchnurseryData.business_name}</h1>
-                </div>
-                <div className='flex items-center justify-between text-center'>
-
-
-                    <h2 className='px-4 text-xl font-bold' >{fetchnurseryData.phone}</h2>
-
+                      <h1 className='items-center pt-6 pl-3 text-xl font-bold' >{fetchnurseryData.business_name}</h1>
                 </div>
 
+            </div>
+
+            <div className='block px-24 mt-4'>
+             <h1 className='items-center pt-6 text-xl font-bold' ><IoLocationOutline className='inline text-2xl'/> Business Location   <span className='ml-2'> ( <MdOutlinePhoneInTalk className='inline text-xl'/>  {fetchnurseryData.phone}</span> ) </h1>
+            <h2 className='items-center justify-center px-4 text-lg ' ></h2>
+
+            <h2 className='px-1 mt-2 text-xl'>{fetchnurseryData.business_location}</h2>
 
             </div>
             
-            <div className='items-center block mt-10 text-center'>
-
-                <h2 className='px-1 text-xl '>{fetchnurseryData.business_location}</h2>
-
-
-            </div>
-
-            <div className='px-24 mt-16 mb-32'>
+            <div className='px-24 mt-6 mb-32 text-lg'>
                 <p class="leading-relaxed" dangerouslySetInnerHTML={{ __html: fetchnurseryData.description }} ></p>
             </div>
-
-
 
         </div>
     )
