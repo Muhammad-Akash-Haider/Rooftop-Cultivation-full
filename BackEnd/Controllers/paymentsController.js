a07a81b8c8c2018f118de322abb6a8f3341b323c
Majid ali <majidali37406@gmail.com>
2024-02-15 11:28:23 +0500

    payment method work done

--- BackEnd/Controllers/paymentsController.js
+++ BackEnd/Controllers/paymentsController.js
@@ -1,40 +1,58 @@
-const express = require('express')
+const express = require("express");
+const stripe = require("stripe")("sk_test_51OjdVEDLpC8Qo70I0YrtXllKMfOrMKoLhlYZmTRzHD5kMdFjvcQkXgrE9Rj22j9v0CyP1EzDv42tWxUEDUqk9h2P00QTkncruY");
 
-const connection = require('../Config/db')
-
-
-
+const connection = require("../Config/db");
 
 //  Get All payment history
 exports.paymenthistory = async (req, res) => {
-
-
-    connection.query('SELECT * FROM `payments_history`', (err, rows, fields) => {
-        if (!err) {
-            res.json({
-                rows
-            })
-        }
-
-        else
-            console.log(err);
-    })
-}
-
+  connection.query("SELECT * FROM `payments_history`", (err, rows, fields) => {
+    if (!err) {
+      res.json({
+        rows,
+      });
+    } else console.log(err);
+  });
+};
 
 exports.getpaymentsbyId = async (req, res) => {
-    console.log(req.params.id)
+  console.log(req.params.id);
+
+  connection.query(
+    "SELECT * FROM `payments_history` WHERE seller_id = " + req.params.id,
+    (err, rows, fields) => {
+      if (!err) {
+        res.json({
+          rows,
+        });
+      } else console.log(err);
+    }
+  );
+};
+
+exports.Makepayment = async (req, res) => {
+//   console.log(req.body);
+
+  const {products} = req.body;
 
 
-    connection.query('SELECT * FROM `payments_history` WHERE seller_id = ' + req.params.id, (err, rows, fields) => {
-        if (!err) {
-            res.json({
-                rows,
-               
-            })
-        }
+    const lineItems = products.map((product)=>({
+        price_data:{
+            currency:"pkr",
+            product_data:{
+                name:product.name
+            },
+            unit_amount:product.price  * 100 ,
+        },
+        quantity:product.stock
+    }));
 
-        else
-            console.log(err);
-    })
-}
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types:["card"],
+        line_items:lineItems,
+        mode:"payment",
+        success_url:"http://localhost:3000/sucess",
+        cancel_url:"http://localhost:3000/cancel",
+    });
+
+    res.json({id:session.id})
+};
--- BackEnd/Routes/paymentsRoutes.js
+++ BackEnd/Routes/paymentsRoutes.js
@@ -1,12 +1,13 @@
 const express= require('express')
 const router = express.Router()
 
-const{ paymenthistory ,getpaymentsbyId}=require('../Controllers/paymentsController')
+const{ paymenthistory ,getpaymentsbyId,Makepayment}=require('../Controllers/paymentsController')
 
 //Get Request All and By ID
 // router.route('/get/Users').get(getUsers)
 
 
+router.route('/Makepayment').post(Makepayment)
 router.route('/get').get(paymenthistory)
 router.route('/get/:id').get(getpaymentsbyId)
 
