93f733141d4e7c82efade701f972aedcf948c732
Majid ali <majidali37406@gmail.com>
2024-02-17 20:34:51 +0500

    work done to get session id

--- BackEnd/Controllers/paymentsController.js
+++ BackEnd/Controllers/paymentsController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const stripe = require("stripe")("sk_test_51OjdVEDLpC8Qo70I0YrtXllKMfOrMKoLhlYZmTRzHD5kMdFjvcQkXgrE9Rj22j9v0CyP1EzDv42tWxUEDUqk9h2P00QTkncruY");
-
+const url = require('url');
 const connection = require("../Config/db");
 
 //  Get All payment history
@@ -50,9 +50,22 @@ exports.Makepayment = async (req, res) => {
         payment_method_types:["card"],
         line_items:lineItems,
         mode:"payment",
-        success_url:"http://localhost:3000/sucess",
+        success_url:"http://localhost:5000/payment/saveorder/{CHECKOUT_SESSION_ID}",
         cancel_url:"http://localhost:3000/cancel",
     });
 
-    res.json({id:session.id})
+    res.json({ id: session.id, url: session.url });
+};
+
+
+exports.handleWebhook = async (req, res) => {
+  console.log(req.params,'i have goten the session id and all work doen in payment')
+  res.send(req.params)
+  // const sessionId = req.query.session_id;
+  // const session = await stripe.checkout.sessions.retrieve(sessionId);
+  // const paymentId = session.payment_intent;
+  // // Now you can store the payment ID in your database
+  // console.log('Payment ID:', paymentId);
+  // // You can write code here to store the payment ID in your database
+  // res.send('Payment successful! Payment ID: ' + paymentId);
 };
--- BackEnd/Routes/paymentsRoutes.js
+++ BackEnd/Routes/paymentsRoutes.js
@@ -1,12 +1,13 @@
 const express= require('express')
 const router = express.Router()
 
-const{ paymenthistory ,getpaymentsbyId,Makepayment}=require('../Controllers/paymentsController')
+const{ paymenthistory ,getpaymentsbyId,Makepayment,handleWebhook}=require('../Controllers/paymentsController')
 
 //Get Request All and By ID
 // router.route('/get/Users').get(getUsers)
 
 
+router.route('/saveorder/:session_id').get(handleWebhook)
 router.route('/Makepayment').post(Makepayment)
 router.route('/get').get(paymenthistory)
 router.route('/get/:id').get(getpaymentsbyId)
