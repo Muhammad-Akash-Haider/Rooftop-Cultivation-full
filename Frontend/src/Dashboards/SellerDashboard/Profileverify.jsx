b5fd97c48dfd2e34f59991f14ea0962a696d25d8
Majid ali <majidali37406@gmail.com>
2024-03-01 15:51:45 +0500

    frontend done

--- Frontend/src/Dashboards/SellerDashboard/Profileverify.jsx
+++ Frontend/src/Dashboards/SellerDashboard/Profileverify.jsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState ,useRef } from 'react';
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { FaBars } from 'react-icons/fa';
