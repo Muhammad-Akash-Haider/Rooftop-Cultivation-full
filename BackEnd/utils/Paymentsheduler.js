3b2f9cf5aea5c53f1d1617b353247378de5a444d
Majid ali <majidali37406@gmail.com>
2024-03-24 14:17:29 +0500

    some important changes in crown jobs

--- BackEnd/utils/Paymentsheduler.js
+++ BackEnd/utils/Paymentsheduler.js
@@ -2,35 +2,35 @@ const schedule = require('node-schedule');
 const connection = require('../Config/db')
 
 const job = schedule.scheduleJob('0 */12 * * *', function () {
-    async function transferPayment(paymentId, amount) {
-    
-        const percentage = 0.05; // 5 percent
-        const transferAmount = amount * (1 - percentage); // Calculate the transfer amount
+  async function transferPayment(paymentId, amount) {
 
-        console.log('Transfer Amount:', transferAmount);
-        console.log('Payment ID:', paymentId);
+    const percentage = 0.05; // 5 percent
+    const transferAmount = amount * (1 - percentage); // Calculate the transfer amount
 
-        try {
-          const transfer = await stripe.transfers.create({
-              amount: transferAmount * 100, // Amount in cents
-              currency: 'pkr', // Change to the appropriate currency
-              destination: paymentId, // Seller's connected Stripe account ID
-          });
-  
-          return transfer;
-      } catch (error) {
-          console.error('Transfer error:', error);
-          throw error;
-      }
-    
-}
+    console.log('Transfer Amount:', transferAmount);
+    console.log('Payment ID:', paymentId);
+
+    try {
+      const transfer = await stripe.transfers.create({
+        amount: transferAmount * 100, // Amount in cents
+        currency: 'pkr', // Change to the appropriate currency
+        destination: paymentId, // Seller's connected Stripe account ID
+      });
+
+      return transfer;
+    } catch (error) {
+      console.error('Transfer error:', error);
+      throw error;
+    }
+
+  }
 
 
 
   connection.query('SELECT * FROM orders \
-    INNER JOIN order_items ON orders.id = order_items.order_id \
-    INNER JOIN plant ON plant.id = order_items.product_id \
-    INNER JOIN users ON plant.seller_id = users.id WHERE order_items.status NOT IN ("return", "cancelled")'
+INNER JOIN order_items ON orders.id = order_items.order_id \
+INNER JOIN plant ON plant.id = order_items.product_id \
+INNER JOIN users ON plant.seller_id = users.id WHERE order_items.status NOT IN ("return", "cancelled")'
     , (err, rows, fields) => {
       if (!err) {
         // Filter orders placed 7 days ago and whose payment status is still pending
@@ -49,25 +49,44 @@ const job = schedule.scheduleJob('0 */12 * * *', function () {
               res.status(500).json({ error: 'Database error' }); // Sending a 500 Internal Server Error response
             } else {
               if (results.length > 0) {
-                
+
                 const paymentId = results[0].payment_id;
                 const amount = order.price;
-                
+
                 // transferPayment(paymentId, amount)
                 // .then(refund => {
                 //     console.log('Payment transfer successful:', refund);
-                    
+
                 // })
                 // .catch(error => {
                 //     res.status(500).json({ success: false, error: error.message });
                 // });
-                // connection.query('UPDATE order_items SET status = ? WHERE items_id = ?', ['completed', order.items_id], (updateErr, updateResult) => {
-                //   if (updateErr) {
-                //     console.error(`Error updating payment status for order ${order.items_id}:`, updateErr);
-                //   } else {
-                //     console.log(`Payment status updated for order ${order.items_id}`);
-                //   }
-                // });
+
+                // query for update order status
+
+                connection.query('UPDATE order_items SET status = ? WHERE items_id = ?', ['completed', order.items_id], (updateErr, updateResult) => {
+                  if (updateErr) {
+                    console.error(`Error updating payment status for order ${order.items_id}:`, updateErr);
+                  } else {
+                    console.log(`Payment status updated for order ${order.items_id}`);
+                  }
+                });
+
+                /////query for transation 
+                const currentDate = new Date();
+                const insertQuery = `INSERT INTO payments_history (payment_date, payment_method, order_id, payment_amount )
+                     VALUES (?, ?, ?, ?)`;
+                const values = [ currentDate , 'Bank Transfer', order.items_id, order.price ];
+
+                // Execute the query
+                connection.query(insertQuery, values, (error, results, fields) => {
+                  if (error) {
+                    console.error('Error occurred:', error);
+                    throw error;
+                  }
+                  console.log('Insert successful!');
+                });
+
 
               } else {
 
@@ -91,5 +110,6 @@ const job = schedule.scheduleJob('0 */12 * * *', function () {
         });
       }
     });
+
 
 });
