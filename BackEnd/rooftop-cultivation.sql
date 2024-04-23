b8b9749af08fe52ce35592420f96ae076bc4d937
Majidali343 <majidali37406@gmail.com>
2024-04-23 10:03:36 +0500

    must use thi new database import this new database forst then run the code

--- BackEnd/rooftop-cultivation.sql
+++ BackEnd/rooftop-cultivation.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 14, 2024 at 03:02 PM
+-- Generation Time: Apr 23, 2024 at 07:02 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -57,8 +57,7 @@ CREATE TABLE `bankaccounts` (
 --
 
 INSERT INTO `bankaccounts` (`id`, `seller_id`, `payment_id`) VALUES
-(1, 25, 'pi_3OolkjDLpC8Qo70I2ZznAz5i'),
-(2, 25, 'pi_3OqtWuDLpC8Qo70I2kScDPk2');
+(1, 25, 'pi_3OxMHPDLpC8Qo70I0xsd7Igf');
 
 -- --------------------------------------------------------
 
@@ -78,8 +77,7 @@ CREATE TABLE `cart` (
 --
 
 INSERT INTO `cart` (`id`, `buyer_id`, `product_id`, `stock`) VALUES
-(5, '25', 31, 2),
-(24, '26', 32, 2);
+(5, '25', 31, 2);
 
 -- --------------------------------------------------------
 
@@ -88,11 +86,38 @@ INSERT INTO `cart` (`id`, `buyer_id`, `product_id`, `stock`) VALUES
 --
 
 CREATE TABLE `chat` (
-  `id` int(64) NOT NULL,
+  `chatid` int(64) NOT NULL,
   `sender_id` int(64) NOT NULL,
-  `message` varchar(500) NOT NULL,
-  `time` time(6) NOT NULL
+  `receiver_id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `chat`
+--
+
+INSERT INTO `chat` (`chatid`, `sender_id`, `receiver_id`) VALUES
+(7, 26, 25);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `messageid` int(64) NOT NULL,
+  `chatid` int(64) NOT NULL,
+  `message` varchar(500) NOT NULL,
+  `recieverseen` int(10) NOT NULL DEFAULT 0,
+  `date_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`messageid`, `chatid`, `message`, `recieverseen`, `date_time`) VALUES
+(20, 7, 'hey', 0, '2024-04-22 17:45:06');
 
 -- --------------------------------------------------------
 
@@ -146,18 +171,18 @@ CREATE TABLE `orders` (
   `id` int(64) NOT NULL,
   `buyer_id` int(50) NOT NULL,
   `order_date` datetime DEFAULT NULL,
-  `payment_id` varchar(1000) NOT NULL,
-  `status` varchar(100) NOT NULL
+  `payment_id` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`id`, `buyer_id`, `order_date`, `payment_id`, `status`) VALUES
-(6, 25, '2024-02-21 20:56:52', 'pi_3OmWydDLpC8Qo70I0iQFD2oW', 'Completed'),
-(10, 26, '2024-02-28 18:09:43', 'pi_3OomtCDLpC8Qo70I1f3KTNbT', 'Pending'),
-(32, 26, '2024-03-05 13:37:56', 'pi_3OqtVODLpC8Qo70I1W6iZdqB', 'Pending');
+INSERT INTO `orders` (`id`, `buyer_id`, `order_date`, `payment_id`) VALUES
+(41, 26, '2024-03-23 09:41:59', 'pi_3OomtCDLpC8Qo70I1f3KTNbT'),
+(42, 26, '2024-03-26 11:48:21', 'pi_3OyTnwDLpC8Qo70I0F2L7Bt8'),
+(43, 26, '2024-04-14 13:14:23', 'pi_3P5OCYDLpC8Qo70I0ULUaciJ'),
+(44, 26, '2024-04-22 19:45:44', 'pi_3P8O7iDLpC8Qo70I2iXJFDtY');
 
 -- --------------------------------------------------------
 
@@ -166,26 +191,24 @@ INSERT INTO `orders` (`id`, `buyer_id`, `order_date`, `payment_id`, `status`) VA
 --
 
 CREATE TABLE `order_items` (
-  `id` int(200) NOT NULL,
+  `items_id` int(200) NOT NULL,
   `order_id` int(200) NOT NULL,
   `product_id` int(200) NOT NULL,
-  `quantity` int(200) NOT NULL
+  `quantity` int(200) NOT NULL,
+  `status` varchar(500) NOT NULL DEFAULT 'Pending',
+  `change_date` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `order_items`
 --
 
-INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `quantity`) VALUES
-(1, 4, 31, 2),
-(2, 5, 31, 2),
-(3, 5, 30, 2),
-(4, 6, 31, 1),
-(5, 7, 34, 2),
-(6, 8, 31, 2),
-(7, 9, 31, 1),
-(8, 10, 32, 1),
-(9, 32, 31, 1);
+INSERT INTO `order_items` (`items_id`, `order_id`, `product_id`, `quantity`, `status`, `change_date`) VALUES
+(19, 41, 30, 1, 'Return', '2024-03-24 17:02:01'),
+(20, 41, 34, 1, 'Cancelled', '2024-03-24 15:19:21'),
+(21, 42, 31, 1, 'Completed', '2024-03-26 11:50:48'),
+(22, 43, 30, 1, 'Cancelled', '2024-04-14 14:09:53'),
+(23, 44, 30, 1, 'Pending', NULL);
 
 -- --------------------------------------------------------
 
@@ -195,21 +218,19 @@ INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `quantity`) VALUES
 
 CREATE TABLE `payments_history` (
   `id` int(64) NOT NULL,
-  `buyer_id` int(50) NOT NULL,
-  `payment_date` int(64) NOT NULL,
+  `payment_date` datetime NOT NULL,
   `payment_method` char(128) NOT NULL,
-  `order_ids` varchar(500) NOT NULL,
   `payment_amount` int(6) NOT NULL,
-  `seller_id` int(100) NOT NULL
+  `order_id` int(200) NOT NULL,
+  `seller_id` int(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `payments_history`
 --
 
-INSERT INTO `payments_history` (`id`, `buyer_id`, `payment_date`, `payment_method`, `order_ids`, `payment_amount`, `seller_id`) VALUES
-(1, 0, 20230312, 'jazzcash', '2023-02-12', 2000, 25),
-(2, 0, 20230812, 'jazzcash', '2023-08-12', 2500, 25);
+INSERT INTO `payments_history` (`id`, `payment_date`, `payment_method`, `payment_amount`, `order_id`, `seller_id`) VALUES
+(5, '2024-03-24 14:04:59', 'Bank Transfer', 2500, 19, 25);
 
 -- --------------------------------------------------------
 
@@ -236,10 +257,10 @@ CREATE TABLE `plant` (
 --
 
 INSERT INTO `plant` (`id`, `seller_id`, `name`, `price`, `stock`, `category`, `description`, `images`, `size`, `sensitivity`, `inoutdoor`) VALUES
-(30, '25', 'Iris virginica ', 2500, 7, 'flowers', '<h2><em>Itea virginica</em></h2><h3><em>Itea virginica</em>&nbsp;L.</h3><h3>Virginia Sweetspire, Tassel-white, Virginia Willow</h3><p>Virginia sweetspire is a mound-shaped, slender-branched,&nbsp;<span style=\"color: rgb(103, 118, 23);\">deciduous</span>&nbsp;<span style=\"color: rgb(103, 118, 23);\">shrub</span>&nbsp;to 10 ft. Small, white flowers bloom in 4 in. spires that droop with the arching branches. Flowers open from base to tip so that the plant appears to bloom for a long time. Leaves turn red to purple in fall and persist well into the winter. This plant is&nbsp;<span style=\"color: rgb(103, 118, 23);\">semi-evergreen</span>&nbsp;in the southern part of its range.</p><p>The long tassels of white flowers and red fall foliage make this an attractive ornamental. Most effective in massed plantings, as single plants tend to be scraggly.</p><p>&nbsp;</p>', 'images-1704622464852-images-1701595984146-satosa3).jpeg, images-1704622464852-images-1701596015936-satosa1).jpeg, images-1704622464853-images-1701596015936-satosa3).jpeg', 'medium', 'medium', 'outdoor'),
-(31, '25', 'Setosa', 1400, 2, 'fruits', '<h3><strong>Why to Choose Kasur Nursery Farm Islamabad.</strong></h3><p>If you have a yard, garden or balcony that needs a little love, we can help.</p><p>We offer a wide range of plants in all shapes and sizes—from hardy flowers to tropical trees and shrubs. We also have a large selection of ornamental grasses, succulents and cacti.</p><p>We offer a range of garden design services in Bhara Kahu so you can choose what your garden needs for success. We can build your dream garden with our custom-designed gardens or we can help you bring your vision to life by designing your garden layout on our website.</p>', 'images-1704622593085-images-1701596015936-satosa3).jpeg, images-1701593799952-satosa3).jpeg, images-1701593799952-satosa (2).jpeg', 'medium', 'medium', 'indoor'),
+(30, '25', 'Iris virginica ', 2500, 1, 'flowers', '<h2><em>Itea virginica</em></h2><h3><em>Itea virginica</em>&nbsp;L.</h3><h3>Virginia Sweetspire, Tassel-white, Virginia Willow</h3><p>Virginia sweetspire is a mound-shaped, slender-branched,&nbsp;<span style=\"color: rgb(103, 118, 23);\">deciduous</span>&nbsp;<span style=\"color: rgb(103, 118, 23);\">shrub</span>&nbsp;to 10 ft. Small, white flowers bloom in 4 in. spires that droop with the arching branches. Flowers open from base to tip so that the plant appears to bloom for a long time. Leaves turn red to purple in fall and persist well into the winter. This plant is&nbsp;<span style=\"color: rgb(103, 118, 23);\">semi-evergreen</span>&nbsp;in the southern part of its range.</p><p>The long tassels of white flowers and red fall foliage make this an attractive ornamental. Most effective in massed plantings, as single plants tend to be scraggly.</p><p>&nbsp;</p>', 'images-1704622464852-images-1701595984146-satosa3).jpeg, images-1704622464852-images-1701596015936-satosa1).jpeg, images-1704622464853-images-1701596015936-satosa3).jpeg', 'medium', 'medium', 'outdoor'),
+(31, '25', 'Setosa', 1400, 1, 'fruits', '<h3><strong>Why to Choose Kasur Nursery Farm Islamabad.</strong></h3><p>If you have a yard, garden or balcony that needs a little love, we can help.</p><p>We offer a wide range of plants in all shapes and sizes—from hardy flowers to tropical trees and shrubs. We also have a large selection of ornamental grasses, succulents and cacti.</p><p>We offer a range of garden design services in Bhara Kahu so you can choose what your garden needs for success. We can build your dream garden with our custom-designed gardens or we can help you bring your vision to life by designing your garden layout on our website.</p>', 'images-1704622593085-images-1701596015936-satosa3).jpeg, images-1701593799952-satosa3).jpeg, images-1701593799952-satosa (2).jpeg', 'medium', 'medium', 'indoor'),
 (32, '25', 'Setosa tea', 1800, 2, 'fruits', '<h3><strong>Why to Choose Kasur Nursery Farm Islamabad.</strong></h3><p>If you have a yard, garden or balcony that needs a little love, we can help.</p><p>We offer a wide range of plants in all shapes and sizes—from hardy flowers to tropical trees and shrubs. We also have a large selection of ornamental grasses, succulents and cacti.</p><p>We offer a range of garden design services in Bhara Kahu so you can choose what your garden needs for success. We can build your dream garden with our custom-designed gardens or we can help you bring your vision to life by designing your garden layout on our website.</p>', 'images-1701596015936-satosa (2).jpeg, images-1701596015936-satosa3).jpeg, images-1701596015936-satosa1).jpeg', 'small', 'medium', 'both'),
-(34, '25', 'Red Rose ', 1000, 8, 'flowers', '<p>This is my collection of rose have multiple bariants</p>', 'images-1704882669750-adelaide-hoodless-rose.jpg, images-1704882669750-04.-Rose-Flower-zoom-view-image.jpg', 'medium', 'high', 'outdoor');
+(34, '25', 'Red Rose ', 1000, 1, 'flowers', '<p>This is my collection of rose have multiple bariants</p>', 'images-1704882669750-adelaide-hoodless-rose.jpg, images-1704882669750-04.-Rose-Flower-zoom-view-image.jpg', 'medium', 'high', 'outdoor');
 
 -- --------------------------------------------------------
 
@@ -281,19 +302,21 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `user_type` varchar(20) NOT NULL,
   `city` varchar(100) NOT NULL,
+  `delievery_address` varchar(1000) DEFAULT NULL,
   `phone` varchar(1000) NOT NULL,
-  `status` int(100) DEFAULT 0
+  `user_status` int(100) DEFAULT 0,
+  `otp` int(100) DEFAULT NULL,
+  `email_verified` int(100) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `First_name`, `last_name`, `email`, `password`, `user_type`, `city`, `phone`, `status`) VALUES
-(25, 'Majid ', 'ali', 'ma5788678@gmail.com', '$2b$10$YjXfWrnKDSyYNnGa2yMoROOsX/I2hcRnrXy44pwRAXPazMn0teBXa', '1', 'Rawalpindi', '03185402854', 0),
-(26, 'muhammad ', 'ashba', 'ashba@gmail.com', '$2b$10$YjXfWrnKDSyYNnGa2yMoROOsX/I2hcRnrXy44pwRAXPazMn0teBXa', '0', 'Rawalpindi', '03110794433', 0),
-(34, 'zahid', 'ali', 'testproject@gmail.com', '$2b$10$ReaxTlslYCYIonzfg9wVcecIEq2lUiejmMfl19zPqzbwQ.OzoKxX6', '1', 'Rawalpindi', '03110794433', 0),
-(61, 'majid', 'hassan', 'usermaji@gmail.com', '$2b$10$YjXfWrnKDSyYNnGa2yMoROOsX/I2hcRnrXy44pwRAXPazMn0teBXa', '0', 'Rawalpindi', '03110794433', 0);
+INSERT INTO `users` (`id`, `First_name`, `last_name`, `email`, `password`, `user_type`, `city`, `delievery_address`, `phone`, `user_status`, `otp`, `email_verified`) VALUES
+(25, 'Majid ', 'ali', 'ma5788678@gmail.com', '$2b$10$YjXfWrnKDSyYNnGa2yMoROOsX/I2hcRnrXy44pwRAXPazMn0teBXa', '1', 'Rawalpindi', NULL, '03185402854', 1, 968597, 1),
+(26, 'muhammad ', 'ashba', 'majid.glaxit@gmail.com', '$2b$10$YjXfWrnKDSyYNnGa2yMoROOsX/I2hcRnrXy44pwRAXPazMn0teBXa', '0', 'Rawalpindi', 'wah cantt b block taxila islamabd', '03110794433', 0, 321489, 1),
+(69, 'asfasd ', 'yar', 'asfand@gamil.com', '$2b$10$RFxExNvU/.mSF1CzZGhvFOq339dI1vKNHsUPuq39nY15MN/MvXVkW', '1', 'Islamabad', NULL, '03110794433', 0, 557704, 1);
 
 -- --------------------------------------------------------
 
@@ -329,7 +352,8 @@ ALTER TABLE `admin`
 -- Indexes for table `bankaccounts`
 --
 ALTER TABLE `bankaccounts`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `seller_id` (`seller_id`);
 
 --
 -- Indexes for table `cart`
@@ -341,7 +365,13 @@ ALTER TABLE `cart`
 -- Indexes for table `chat`
 --
 ALTER TABLE `chat`
-  ADD UNIQUE KEY `id` (`id`);
+  ADD UNIQUE KEY `id` (`chatid`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`messageid`);
 
 --
 -- Indexes for table `notifications`
@@ -365,7 +395,7 @@ ALTER TABLE `orders`
 -- Indexes for table `order_items`
 --
 ALTER TABLE `order_items`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`items_id`);
 
 --
 -- Indexes for table `payments_history`
@@ -412,13 +442,25 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `bankaccounts`
 --
 ALTER TABLE `bankaccounts`
-  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT for table `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `chatid` int(64) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `messageid` int(64) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `notifications`
@@ -436,19 +478,19 @@ ALTER TABLE `nursery`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(64) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(64) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `items_id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `payments_history`
 --
 ALTER TABLE `payments_history`
-  MODIFY `id` int(64) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(64) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `plant`
@@ -466,7 +508,7 @@ ALTER TABLE `return/cancels`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(64) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+  MODIFY `id` int(64) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 
 --
 -- AUTO_INCREMENT for table `verification_documents`
